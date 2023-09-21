4093bb7858996e7787b613c73ff9d306b15ed6fc
darkrazor99 <tarektheman99@gmail.com>
2023-09-21 12:59:35 +0200

    added a preloader

--- app.js
+++ app.js
@@ -5,8 +5,14 @@
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
+// preloader
+const preloader = document.querySelector(".preloader");
 
-btn.addEventListener("click", (e) => {
+window.addEventListener("load", () => {
+    preloader.classList.add("hide-preloader");
+});
+
+btn.addEventListener("click", () => {
     if (!btn.classList.contains("slide")) {
         btn.classList.add("slide");
         video.pause();
